f27de72fb3055a60154280bbb3a310f2c4644420
Laura E <Laura E>
2017-02-23 18:47:21 -0500

    It's working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,13 @@
-var pickedButton;
+var pickedButton; //Used below
 
+//Start of the question bank. Playing with different ways of orgainizing things.
 var tie = {
 	question: "What does TIE stand for?",
 	a: "This Infernal Empire",
 	b: "Thermal Interference Engine",
 	c: "Twin Ion Engine",
 	d: "Turbo Impulse Engine",
+	correct:"Twin Ion Engine",
 	answer: function(click){
 		if(pickedButton=="c"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -21,6 +23,7 @@ var pilot = {
 	b: "Han Solo",
 	c: "Lando Calrissian",
 	d: "Wedge Antilles",
+	correct:"Wedge Antilles",
 	answer: function(click){
 		if(pickedButton=="d"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -36,6 +39,7 @@ var ssd = {
 	b: "Relentless",
 	c: "Devastator",
 	d: "Ravager",
+	correct: "Executor",
 	answer: function(click){
 		if(pickedButton=="a"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -50,7 +54,8 @@ var sith = {
 	a: "Darh Maul",
 	b: "Yoko Ono",
 	c: "Emperor Palpatine",
-	d: "Wedge Antilles",
+	d: "Darth Vader",
+	correct: "Yoko Ono",
 	answer: function(click){
 		if(pickedButton=="b"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -66,6 +71,7 @@ var flight = {
 	b: "Blue Two",
 	c: "Green Six",
 	d: "Red Five",
+	correct: "Red Five",
 	answer: function(click){
 		if(pickedButton=="d"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -81,6 +87,7 @@ var gungan = {
 	b: "Otoh Gunga",
 	c: "Otoh Raban",
 	d: "Otoh Sancture",
+	correct: "Otoh Gunga",
 	answer: function(click){
 		if(pickedButton=="b"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -96,6 +103,7 @@ var ds = {
 	b: "Five",
 	c: "Three",
 	d: "Seven",
+	correct: "Three",
 	answer: function(click){
 		if(pickedButton=="c"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -111,6 +119,7 @@ var order = {
 	b: "Order 150",
 	c: "Order 69",
 	d: "Order 6",
+	correct: "Order 66",
 	answer: function(click){
 		if(pickedButton=="a"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -126,6 +135,7 @@ var characters = {
 	b: "Anakin 'Darth Vader' Skywalker and Obi-Wan Kenobi",
 	c: "Chewbacca and Yoda",
 	d: "Artoo and Threepio",
+	correct: "Artoo and Threepio",
 	answer: function(click){
 		if(pickedButton=="d"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -141,6 +151,7 @@ var blade = {
 	b: "Emperor Palpatine",
 	c: "Obi-Wan Kenobi",
 	d: "Qui-Gon Jinn",
+	correct: "Mace Windu",
 	answer: function(click){
 		if(pickedButton=="a"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -156,6 +167,7 @@ var pa = {
 	b: "Jrog Sacul",
 	c: "Lobot",
 	d: "Roons Sewell",
+	correct: "Lobot",
 	answer: function(click){
 		if(pickedButton=="c"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -171,6 +183,7 @@ var jango = {
 	b: "Mace Windu",
 	c: "Yoda",
 	d: "Obi-Wan Kenobi",
+	correct: "Mace Windu",
 	answer: function(click){
 		if(pickedButton=="b"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -186,6 +199,7 @@ var stardust = {
 	b: "The Super Star Destroyers",
 	c: "The Starkiller",
 	d: "The Death Star",
+	correct: "The Death Star",
 	answer: function(click){
 		if(pickedButton=="d"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -201,8 +215,10 @@ var rogues = {
 	b: "Bodhi Rook",
 	c: "Zev Senesca",
 	d: "Aril Nunb",
+	correct: "Bodhi Rook",
 	answer: function(click){
-		if(picked=="b"){
+		console.log(this.id);
+		if(pickedButton=="b"){
 			$("#gameplay").html("<h3>That's right!</h3>");
 		} else {
 			$("#gameplay").html("<h3>That's wrong. The correct answer is " + rogues.b + ".</h3>");
@@ -216,6 +232,7 @@ var color = {
 	b: "Blue",
 	c: "Yellow",
 	d: "Red",
+	correct: "Green",
 	answer: function(click){
 		if(pickedButton=="a"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -231,6 +248,7 @@ var falcon = {
 	b: "Han Solo",
 	c: "Luke Skywalker",
 	d: "Leia Organa",
+	correct: "Luke Skywalker",
 	answer: function(click){
 		if(pickedButton=="c"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -246,6 +264,7 @@ var ships = {
 	b: "TIE fighters",
 	c: "Y-wings",
 	d: "A-wings",
+	correct: "A-wings",
 	answer: function(click){
 		if(pickedButton=="d"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -261,6 +280,7 @@ var plans = {
 	b: "Mustafar",
 	c: "Scarif",
 	d: "Jedha",
+	correct: "Scarif",
 	answer: function(click){
 		if(pickedButton=="c"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -276,6 +296,7 @@ var ewok = {
 	b: "Wickett",
 	c: "Paploo",
 	d: "Teebo",
+	correct: "Wickett",
 	answer: function(click){
 		if(pickedButton=="b"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -291,6 +312,7 @@ var trooper = {
 	b: "THX-1138",
 	c: "TK-421",
 	d: "BT-445",
+	correct: "FN-2187",
 	answer: function(click){
 		if(pickedButton=="a"){
 			$("#gameplay").html("<h3>That's right!</h3>");
@@ -298,20 +320,48 @@ var trooper = {
 			$("#gameplay").html("<h3>That's wrong. The correct answer is " + trooper.a + ".</h3>");
 		}
 	},
-};
+}; //End of question bank.
 
-var count = 0;
+var number = 30;
+
+var count = 0; //To go through the questions list.
 
 var questionBank = [tie, pilot, ssd, sith, flight, gungan, ds, order, characters, blade, pa, jango, stardust, rogues, color, falcon, ships, plans, ewok, trooper];
 
-var questions = [];
+var questions = []; //Empty for now
 
+var nextUp;
+
+ function follow(){
+ 	intervalId = setInterval(decrement, 1000);
+ }
+
+function decrement(){
+	number--;
+	$("#display").html("<h2>" + number + "</h2>");
+	if(number===0){
+		console.log("It's working! It's working!");
+		stop();
+		$("#gameplay").html("<h3>Time's up! The correct answer is "+questions[count].correct);
+		setTimeout(newQuestion, 5000);
+	}
+}
+
+function stop(){
+	clearInterval(intervalId); 
+}
+
+//Reset function.
 function reset(){
 	questions = [];
 	$("#gameplay").html("<h3>Click the button to begin.</h3><button type='start' class='btn btn-success h4' id='start'>Start</button>");
 	picked();
+	stop();
+	number = 30;
+	count = 0;
 }
 
+//This picks the what's in questions[].
 function picked(){
 	for (var i = 0; i< 19 ; i++) {
 		var random = Math.floor(Math.random()*20);
@@ -319,12 +369,20 @@ function picked(){
 		if(questions.indexOf(pick)<0 && questions.length<10){
 			questions.push(pick);
 		};	
+		console.log(questions);
 	};
 }
 
-function playGame(){
+//I set this as a function and not a for loop to see other ways I might 
+//orgainize things. It also allows me to call the function several times.
+function setUp(){
 	var spot =$("<div>");
 	var asked = $("<h3>");
+	var timer = $("<div>");
+	timer.attr("id", "display");
+	timer.append("<h2>" + number + "<h2>");
+	follow();
+	spot.append(timer);
 	asked.append(questions[count].question);
 	spot.append(asked);
 	var choices =$("<div>");
@@ -336,13 +394,54 @@ function playGame(){
 	$("#gameplay").html(spot);
 }
 
+function playGame(){
+	var spot =$("<div>");
+	var asked = $("<h3>");
+	var timer = $("<div>");
+	timer.attr("id", "display");
+	timer.append("<h2>" + number + "<h2>");
+	spot.append(timer);
+	asked.append(questions[count].question);
+	spot.append(asked);
+	var choices =$("<div>");
+	choices.append("<button class='btn-success answers' id='a'>" + questions[count].a+"</button>");
+	choices.append("<button class = 'btn-success answers' id='b'>" + questions[count].b+"</button>");
+	choices.append("<button class='btn-success answers' id='c'>" + questions[count].c+"</button>");
+	choices.append("<button class='btn-success answers' id='d'>" + questions[count].d+"</button>");
+	spot.append(choices);
+	$("#gameplay").html(spot);
+}
+
+function changeQuestion(){
+	console.log(count);
+	if (count===questions.length){
+		$("#gameplay").html("<h3>Thanks for playing! Click the button to play again!</h3><button type='replay' class='btn btn-success h4' id= 'replay'>Play Again</button>");
+		clearInterval(nextUp);
+	}
+}
+
+function newQuestion(){
+	nextUp = setTimeout(changeQuestion, 5000);
+	count++;
+	playGame();
+	number=30;
+	follow()
+}
+
 $(document).ready(function(){
+	count=0;
 	picked();
 	$("#gameplay").html("<h3>Click the button to begin.</h3><button type='start' class='btn btn-success h4' id= 'start'>Start</button>");
-	$(document).on("click", "#start", playGame);
+	$(document).on("click", "#start", setUp);
 	$(document).on("click", ".answers", function(){
+		stop();
 		pickedButton = $(this).attr("id");
 		questions[count].answer();
+		setTimeout(newQuestion, 5000);
+	});
+	
+	$(document).on("click", "#replay", function(){
+		reset();
 	});
 	
 });
